e53e64016f42c379ba7d9fd159d1a2bb540093bb
imdp4 <32986857+imdp4@users.noreply.github.com>
2018-05-28 13:36:53 +0530

    Update Arithmetic Operators.py

--- Python/Challenges/Introduction/Arithmetic Operators.py
+++ Python/Challenges/Introduction/Arithmetic Operators.py
@@ -1,37 +1,36 @@
-Arithmetic Operators
+# Arithmetic Operators
 
-Task 
-Read two integers from STDIN and print three lines where:
+# Task 
+# Read two integers from STDIN and print three lines where:
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
 
-The first line contains the sum of the two numbers.
-The second line contains the difference of the two numbers (first - second).
-The third line contains the product of the two numbers.
+# Input Format
+# The first line contains the first integer, a. The second line contains the second integer, b.
 
-Input Format
-The first line contains the first integer, a. The second line contains the second integer, b.
+# Constraints
+# 1 <= a <= 10**10
+# 1 <= b <= 10**10
+ 
+# Output Format
+# Print the three lines as explained above.
 
-Constraints
-1 <= a <= 10**10
-1 <= b <= 10**10
-
-Output Format
-
-Print the three lines as explained above.
-
-Sample Input 0
-
+#Sample Input 0
 3
 2
-Sample Output 0
 
+#Sample Output 0
 5
 1
 6
 
-Explanation 0
+#Explanation 0
 3+2 => 5
 3-2 => 1
 3*2 => 6
+
+#Solution
 
 if __name__ == '__main__':
     a = int(raw_input())
@@ -40,4 +39,3 @@ if __name__ == '__main__':
     print a+b
     print a-b
     print a*b
-    
