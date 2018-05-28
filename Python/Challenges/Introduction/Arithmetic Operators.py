cec9fc6ab2e32171ae5b77715b2db10f32a57356
imdp4 <32986857+imdp4@users.noreply.github.com>
2018-05-28 13:38:08 +0530

    Update Arithmetic Operators.py

--- Python/Challenges/Introduction/Arithmetic Operators.py
+++ Python/Challenges/Introduction/Arithmetic Operators.py
@@ -1,6 +1,7 @@
 # Arithmetic Operators
 
 # Task 
+
 # Read two integers from STDIN and print three lines where:
 # The first line contains the sum of the two numbers.
 # The second line contains the difference of the two numbers (first - second).
