69cbabbbf450fe59116dfc3a517e25239c6ad7b2
imdp4 <32986857+imdp4@users.noreply.github.com>
2018-05-28 13:37:26 +0530

    Update Arithmetic Operators.py

--- Python/Challenges/Introduction/Arithmetic Operators.py
+++ Python/Challenges/Introduction/Arithmetic Operators.py
@@ -16,20 +16,6 @@
 # Output Format
 # Print the three lines as explained above.
 
-#Sample Input 0
-3
-2
-
-#Sample Output 0
-5
-1
-6
-
-#Explanation 0
-3+2 => 5
-3-2 => 1
-3*2 => 6
-
 #Solution
 
 if __name__ == '__main__':
