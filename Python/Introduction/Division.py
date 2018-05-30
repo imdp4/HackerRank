21f863122674289b1cf852c9923450f8766bed72
imdp4 <32986857+imdp4@users.noreply.github.com>
2018-05-30 12:25:59 +0530

    Update Division.py

--- Python/Introduction/Division.py
+++ Python/Introduction/Division.py
@@ -1,22 +1,22 @@
-Task 
-Read two integers and print two lines. The first line should contain integer division, a // b. The second line should contain float division, a / b.
-You don't need to perform any rounding or formatting operations.
+#Task 
+#Read two integers and print two lines. The first line should contain integer division, a // b. The second line should contain float division, a / b.
+#You don't need to perform any rounding or formatting operations.
 
-Input Format
-The first line contains the first integer, a. The second line contains the second integer, b.
+#Input Format
+#The first line contains the first integer, a. The second line contains the second integer, b.
 
-Output Format
-Print the two lines as described above.
+#Output Format
+#Print the two lines as described above.
 
-Sample Input 0
-4
-3
+#Sample Input 0
+#4
+#3
 
-Sample Output 0
-1
-1.33333333333
+#Sample Output 0
+#1
+#1.33333333333
 
-Solution:
+##Solution:
 
 from __future__ import division
 
