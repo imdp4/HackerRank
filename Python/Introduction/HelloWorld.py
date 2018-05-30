43d3fb2b46469d2aa5dc838c5cfd8bb98cd38575
imdp4 <32986857+imdp4@users.noreply.github.com>
2018-05-30 12:30:39 +0530

    Update HelloWorld.py

--- Python/Introduction/HelloWorld.py
+++ Python/Introduction/HelloWorld.py
@@ -1,5 +1,5 @@
+"""
 Here is a sample line of code that can be executed in Python:
-
 print("Hello, World!")
 You can just as easily store a string as a variable and then print it to stdout:
 
@@ -8,18 +8,16 @@ print(my_string)
 The above code will print Hello, World! on your screen. Try it yourself in the editor below!
 
 Input Format
-
 You do not need to read any input in this challenge.
 
 Output Format
-
 Print Hello, World! to stdout.
 
 Sample Output 0
-
 Hello, World!
 
-Solution:
+"""
+# Solution:
 
 if __name__ == '__main__':
     print("Hello, World!")
