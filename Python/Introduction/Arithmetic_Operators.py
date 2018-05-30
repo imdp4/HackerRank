845ecd43c90f3fb89b511d1792cdc13fe70c8872
imdp4 <32986857+imdp4@users.noreply.github.com>
2018-05-30 12:32:14 +0530

    Update Arithmetic_Operators.py

--- Python/Introduction/Arithmetic_Operators.py
+++ Python/Introduction/Arithmetic_Operators.py
@@ -1,28 +1,28 @@
-# Arithmetic Operators
+"""
+Arithmetic Operators
 
-# Task 
+Task 
 
-# Read two integers from STDIN and print three lines where:
-# The first line contains the sum of the two numbers.
-# The second line contains the difference of the two numbers (first - second).
-# The third line contains the product of the two numbers.
+Read two integers from STDIN and print three lines where:
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
 
-# Input Format
-# The first line contains the first integer, a. The second line contains the second integer, b.
+Input Format
+The first line contains the first integer, a. The second line contains the second integer, b.
 
-# Constraints
-# 1 <= a <= 10**10
-# 1 <= b <= 10**10
+Constraints
+1 <= a <= 10**10
+1 <= b <= 10**10
  
-# Output Format
-# Print the three lines as explained above.
-
-#Solution
+Output Format
+Print the three lines as explained above.
+"""
+# Solution
 
 if __name__ == '__main__':
     a = int(raw_input())
     b = int(raw_input())
-    
     print a+b
     print a-b
     print a*b
